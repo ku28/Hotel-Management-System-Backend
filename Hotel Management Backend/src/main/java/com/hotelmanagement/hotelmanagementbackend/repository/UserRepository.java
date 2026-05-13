8de528de24785653b0a12927563c4a8be3fc9d9a
hiteshsaini2332 <hiteshsaini9466@gmail.com>
2026-05-13 15:50:18 +0530

    User Entity and Repository Added

---
+++ Hotel Management Backend/src/main/java/com/hotelmanagement/hotelmanagementbackend/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.hotelmanagement.hotelmanagementbackend.auth.repository;
+
+import com.hotelmanagement.hotelmanagementbackend.auth.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    Page<User> findByRole(String role, Pageable pageable);
+}
