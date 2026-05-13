c134d883b07c3e4a0eb249dd1a866ec23c13334e
AnshulJamwal17 <anshuljamwal90@gmail.com>
2026-05-13 14:27:58 +0530

    Revert "Payment Entity Added"
    This reverts commit 7aed629cca3ff59e8ff54a111422c2298984c8e3.

--- Hotel Management Backend/src/main/java/com/hotelmanagement/hotelmanagementbackend/Enum/PaymentStatus.java
+++
@@ -1,4 +0,0 @@
-package com.hotelmanagement.hotelmanagementbackend.Enum;
-
-public enum PaymentStatus {
-}
--- Hotel Management Backend/src/main/java/com/hotelmanagement/hotelmanagementbackend/HotelManagementBackendApplication.java
+++ Hotel Management Backend/src/main/java/com/hotelmanagement/hotelmanagementbackend/HotelManagementBackendApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HotelManagementBackendApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(HotelManagementBackendApplication.class, args);
     }
 
