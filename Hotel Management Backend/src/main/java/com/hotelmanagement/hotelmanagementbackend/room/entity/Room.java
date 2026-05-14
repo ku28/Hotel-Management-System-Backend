23ed34a74d54e95463ea6acc5dfa9218107a61de
adityakapila9 <adityakapila9@gmail.com>
2026-05-14 10:10:46 +0530

    Rooms and Roomtype Entity and Repo added

--- Hotel Management Backend/src/main/java/com/hotelmanagement/hotelmanagementbackend/room/entity/Room.java
+++ Hotel Management Backend/src/main/java/com/hotelmanagement/hotelmanagementbackend/room/entity/Room.java
@@ -1,25 +1,16 @@
 package com.hotelmanagement.hotelmanagementbackend.room.entity;
 
-import com.hotelmanagement.hotelmanagementbackend.hotel.entity.Amenity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Entity
 @Table(name = "Room")
@@ -43,6 +34,5 @@ public class Room {
     private Boolean isAvailable;
 
     //add join x1
-    @Builder.Default
-    private Set<Amenity> amenities = new HashSet<>();
+   //to do amenity
 }
