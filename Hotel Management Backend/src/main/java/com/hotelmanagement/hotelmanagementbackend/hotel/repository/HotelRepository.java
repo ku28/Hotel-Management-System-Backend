46860615f68cdff48f37d3de1a5dd23dd39323bf
Purab Vats <purabvats18@gmail.com>
2026-05-14 10:07:35 +0530

    Hotel

---
+++ Hotel Management Backend/src/main/java/com/hotelmanagement/hotelmanagementbackend/hotel/repository/HotelRepository.java
@@ -0,0 +1,20 @@
+package com.hotelmanagement.hotelmanagementbackend.hotel.repository;
+
+import com.hotelmanagement.hotelmanagementbackend.hotel.entity.Hotel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel, Integer> {
+
+    Page<Hotel> findByLocationContainingIgnoreCase(String location, Pageable pageable);
+
+    Page<Hotel> findByNameContainingIgnoreCase(String name, Pageable pageable);
+
+    Page<Hotel> findByNameContainingIgnoreCaseOrLocationContainingIgnoreCase(
+            String name, String location, Pageable pageable);
+
+    boolean existsByNameAndLocation(String name, String location);
+}
